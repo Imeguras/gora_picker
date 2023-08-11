252bb4aee38a25519544144c8370b24f1d55914a
Imeguras <imeguras@teknik.io>
2023-08-11 20:55:51 +0100

    Added countries

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -55,39 +55,112 @@ defmodule Start_Seeding do
   end
     list_countries = GoraPicker.Origin.list_countries()
   if list_countries != [] do
-    ids = Enum.map(list_comp, &(&1.id))
-    IO.puts "Comp_package_types already created"
+    ids = Enum.map(list_countries, &(&1.id))
+    IO.puts "Countries already created"
+  else
+    pla = &(Seeding_State.place_data(agent_state, :country_ids, &1))
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "Portugal",
+        "pri_abreviation" => "PT",
+        "location" => "38.71667 -9.13333"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "United States",
+        "pri_abreviation" => "US",
+        "location" => "38.889805 -77.009056"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "China",
+        "pri_abreviation" => "CN",
+        "location" => "39.916668 116.383331"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "Japan",
+        "pri_abreviation" => "JP",
+        "location" => "35.689506 139.6917"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "Germany",
+        "pri_abreviation" => "DE",
+        "location" => "52.516666 13.4"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "United Kingdom",
+        "pri_abreviation" => "GB",
+        "location" => "51.50853 -0.12574"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "France",
+        "pri_abreviation" => "FR",
+        "location" => "48.853409 2.3488"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "Italy",
+        "pri_abreviation" => "IT",
+        "location" => "41.89193 12.51133"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "Spain",
+        "pri_abreviation" => "ES",
+        "location" => "40.4165 -3.70256"
+      })
+      pla.(cs.id)
+    {:ok, cs} =
+      GoraPicker.Origin.create_country(%{
+        "name" => "Canada",
+        "pri_abreviation" => "CA",
+        "location" => "45.508839 -73.587807"
+      })
+      pla.(cs.id)
   end
 
-    list_comp = GoraPicker.Features.list_comp_package_types()
-    if list_comp != [] do
-      #damn first timer, guess im not that bad in functional kek...
-      ids = Enum.map(list_comp, &(&1.id))
-      IO.puts "Comp_package_types already created"
 
-    else
-      pla = & (Seeding_State.place_data(agent_state, :package_type_ids, &1))
-      {:ok, cs} =
-        GoraPicker.Features.create_package__type(%{
-          "name" => "Through Hole"
-        })
-        pla.(cs.id)
+  list_comp = GoraPicker.Features.list_comp_package_types()
+  if list_comp != [] do
+    #damn first timer, guess im not that bad in functional kek...
+    ids = Enum.map(list_comp, &(&1.id))
+    IO.puts "Comp_package_types already created"
 
-      {:ok, cs} =
-        GoraPicker.Features.create_package__type(%{
-          "name" => "Surface Mount"
-        })
-        pla.(cs.id)
+  else
+    pla = & (Seeding_State.place_data(agent_state, :package_type_ids, &1))
+    {:ok, cs} =
+      GoraPicker.Features.create_package__type(%{
+        "name" => "Through Hole"
+      })
+      pla.(cs.id)
 
-      {:ok, cs} =
-        GoraPicker.Features.create_package__type(%{
-          "name" => "Other"
-        })
-        pla.(cs.id)
-    end
+    {:ok, cs} =
+      GoraPicker.Features.create_package__type(%{
+        "name" => "Surface Mount"
+      })
+      pla.(cs.id)
+
+    {:ok, cs} =
+      GoraPicker.Features.create_package__type(%{
+        "name" => "Other"
+      })
+      pla.(cs.id)
+  end
 
 
-  # map through data array and do (GoraPicker.Features.get_package__type!().name))) and replace id with name
+# map through data array and do (GoraPicker.Features.get_package__type!().name))) and replace id with name
   #
   #|> IO.inspect
 
